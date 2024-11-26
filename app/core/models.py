b136a8bd7f8ddc67aa0e2b8436f26dc652dac108
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2024-11-26 21:42:59 +1100

    custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,31 @@
-# from django.db import models
+# Database models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    # Manager for users
+    def create_user(self, email, password=None, **kwargs):
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(
+            using=self._db
+        )  # default database to use. It's the db we defined in settings.py
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # Can login to django admin
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"  # Replace the default field from username to email
