e14d5d8bc25e653db0a5bf833e6553e4b9ca39d2
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2024-12-02 16:30:47 +1100

    proxy docker file & refactor bash script

--- proxy/run.sh
+++ proxy/run.sh
@@ -5,7 +5,7 @@ set -e
 
 # Substitute environment variables into the Nginx configuration template
 # and save the result to the Nginx configuration directory
-envsubst </etc/nginx/default.conf.tpl >/etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
