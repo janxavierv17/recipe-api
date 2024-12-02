bf14a2c1bf3268009bfab4a0f5099f16d11fde58
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2024-12-02 22:13:45 +1100

    add health check endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,9 +22,11 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+from core import views as core_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/health-check", core_views.health_check, name="health-check"),
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path("api/user/", include("user.urls")),
     path("api/recipe/", include("recipe.urls")),
